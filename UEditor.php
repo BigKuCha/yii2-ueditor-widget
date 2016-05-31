0eb5ff018ec95a9c1d2224dc79189cdd39718d56
mackong <mackonghp@gmail.com>
2016-05-31 17:45:41 +0800

    fix(UEditor.php): support custom id from options

--- UEditor.php
+++ UEditor.php
@@ -27,7 +27,12 @@ class UEditor extends InputWidget
      */
     public function init()
     {
-        $this->id = $this->hasModel() ? Html::getInputId($this->model, $this->attribute) : $this->id;
+        if (isset($this->options['id'])) {
+            $this->id = $this->options['id'];
+        } else {
+            $this->id = $this->hasModel() ? Html::getInputId($this->model,
+                $this->attribute) : $this->id;
+        }
         $this->_options = [
             'serverUrl' => Url::to(['upload']),
             'initialFrameWidth' => '100%',
